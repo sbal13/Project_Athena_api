508fb966e8498a68f80c01d23930398aafe4bdbd
sbal13 <sbal13@gmail.com>
2017-10-04 16:51:26 -0400

    fixing cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://athena-academics.herokuapp.com/'
+    origins 'athena-academics.herokuapp.com'
 
     resource '*',
       headers: :any,
