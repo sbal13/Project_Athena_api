39cb281ba6c9133ba178b6325db4b4cf36573063
sbal13 <sbal13@gmail.com>
2017-10-05 14:07:35 -0400

    reverse on copy 2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'athena-academics.herokuapp.com'
-    origins 'http://localhost:3001'
+    origins 'athena-academics.herokuapp.com'
+    # origins 'http://localhost:3001'
 
     resource '*',
       headers: :any,
